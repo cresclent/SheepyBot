4935a05ce05df4de9395d395f2c9e29c4678c774
Cresclent <moonwalker66688@gmail.com>
2026-06-24 13:23:48 +0200

    Add copyright and usage restrictions to ServerTracker
    Added copyright notice and usage restrictions to ServerTracker.cs

--- SmallDat/ServerTracker.cs
+++ SmallDat/ServerTracker.cs
@@ -1,4 +1,6 @@
-﻿using NetCord;
+﻿// Copyright (c) 2026 Cresclent. All rights reserved.
+// This Discord bot code is view-only. Hosting or running this bot is strictly prohibited!
+using NetCord;
 using NetCord.Gateway;
 using discord_bot.userdataModels;
 using System;
